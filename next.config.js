6b2a78d644edec50fea1672ff741f21d72ff9f3a
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2025-03-11 13:11:09 -0700

    committing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Static export
+  distDir: 'out',    // Output folder is "out"
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
+  basePath: '/wellspring', // This should be the name of your repository
+  trailingSlash: true,     // Ensures proper routing in static mode
 };
 
 module.exports = nextConfig;
